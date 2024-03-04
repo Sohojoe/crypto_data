ff2e1e6c047c55bd0534f2e1f8f8eeaf19e09d46
Joe Booth <joe@joebooth.com>
2024-03-03 22:08:46 -0800

    refactor so we can run tests etc

---
+++ setup.py
@@ -0,0 +1,9 @@
+# setup.py at the root of your project
+from setuptools import setup, find_packages
+
+setup(
+    name="crypto_data",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)
